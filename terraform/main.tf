f18c43e3c321c8a730bf396da32bf97063f81a58
Kitsuya0828 <kitsuyaazuma@gmail.com>
2023-04-24 16:52:25 +0900

    fix: use VM with 2 vCPUs

--- terraform/main.tf
+++ terraform/main.tf
@@ -101,7 +101,8 @@ resource "azurerm_linux_virtual_machine" "my_terraform_vm" {
   location              = azurerm_resource_group.rg.location
   resource_group_name   = azurerm_resource_group.rg.name
   network_interface_ids = [azurerm_network_interface.my_terraform_nic.id]
-  size                  = "Standard_DS1_v2"
+  # https://learn.microsoft.com/ja-jp/azure/virtual-machines/acu
+  size                  = "Standard_D2s_v3"
 
   os_disk {
     name                 = "myOsDisk"
